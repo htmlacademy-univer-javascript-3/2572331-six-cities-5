450d3309780013488295bfd0c9144393643acc3c
Ilya <92204295+D3RPole96@users.noreply.github.com>
2024-11-12 10:30:52 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/2572331-six-cities-5",
+  base: "2572331-six-cities-5",
   plugins: [react()],
   test: {
     globals: true,
